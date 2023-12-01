ce41703a66132b0c6413db729b9bb77b8536f1cb
leonardo-fr <leofrabelo2001@gmail.com>
2023-11-30 23:02:22 -0300

    Criando a classe Contato

---
+++ src/main/java/model/Contato.java
@@ -0,0 +1,17 @@
+public class Contato {
+    private String email;
+    private String celular;
+
+    public Contato(String email, String celular) {
+        this.email = email;
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+}
